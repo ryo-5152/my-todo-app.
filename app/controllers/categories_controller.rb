d00ed93b92b9fd96dd4c00d7c87f99136e5aed73
Ubuntu <ubuntu@ip-172-31-14-45.ap-northeast-1.compute.internal>
2020-10-03 10:02:45 +0000

    todoapp

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    redirect_to categories_path
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to categories_path
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:title)
+    end
+end
+
