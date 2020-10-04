60d8aaf8044373e07ba3bc1fb822d45283349f4e
Ubuntu <ubuntu@ip-172-31-14-45.ap-northeast-1.compute.internal>
2020-10-04 15:54:27 +0000

    fixed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-   before_action :correct_user,   only: :destroy
+
 
    def create
        @category = Category.find_by(params[:id])
@@ -27,6 +27,9 @@ class TodosController < ApplicationController
    end
    
    def destroy
+      @category = Category.find_by(params[:id])
+      @todo =@category.todos.find_by(id: params[:id])
+      
        @todo.destroy
        redirect_to @category
    end
@@ -37,11 +40,7 @@ class TodosController < ApplicationController
       params.require(:todo).permit(:content)
     end
     
-    def correct_user
-      @category = Category.find_by(params[:id])
-      @todo =@category.todos.find_by(id: params[:id])
-      redirect_to root_url if @todo.nil?
-    end
+
     
 end
     
